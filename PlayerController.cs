d5ba04f08f4680009333b45c6f9dea35e2d2c128
Riley Stange <52304971+rjstange@users.noreply.github.com>
2020-01-09 23:19:17 -0600

    First version with basic AI
    Next version will include ML Agents trained AI and a black opposing cylinder to increase the challenge.

---
+++ PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 140.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * horizontalInput);
+        playerRb.AddForce(Vector3.left * speed * verticalInput);
+    }
+}
